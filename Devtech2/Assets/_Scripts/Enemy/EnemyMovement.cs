9891317f50b43750982cea49538a401d614759dd
Gustavo Rocha <gprieto@neurovirtual.com>
2024-06-18 18:05:05 -0300

    Start

---
+++ Devtech2/Assets/_Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4.0f;
+    [SerializeField] private Vector2 _movement;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
