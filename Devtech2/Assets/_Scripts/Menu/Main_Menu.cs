e46247e91ad9dd544632de7ba18713e5f2232a12
Lup38 <105895723+Lup38@users.noreply.github.com>
2024-06-19 22:04:34 -0300

    add main menu

---
+++ Devtech2/Assets/_Scripts/Menu/Main_Menu.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject creditos;
+    [SerializeField] private GameObject config;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+
+
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    #region activation
+    //parte de ativação ode menus
+    public void ActivateCredits()
+    {
+        if (creditos.activeSelf == true)
+        {
+            verifyActive();
+        }
+        else
+        {
+            verifyActive();
+            creditos.SetActive(true);
+        }
+
+    }
+
+    public void ActivateConfig()
+    {
+        if (config.activeSelf == true)
+        {
+            verifyActive();
+        }
+        else
+        {
+            verifyActive();
+            config.SetActive(true);
+        }
+
+
+    }
+
+    private void verifyActive()
+    {
+        if (creditos.activeSelf == true)
+        {
+            creditos.SetActive(false);
+
+        }
+        if (config.activeSelf == true)
+        {
+            config.SetActive(false);
+
+        }
+    }
+    #endregion
+
+
+
+
+}
+
+
+
+
+
