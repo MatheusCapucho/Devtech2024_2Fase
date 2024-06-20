7c2815824cb025fd523dbe6dab6bf2a6fcc35eec
Gustavo Rocha <gprieto@neurovirtual.com>
2024-06-20 11:22:07 -0300

    [IMP] AntiVirus Falling

--- Devtech2/Assets/_Scripts/Antivirus/AntivirusMovement.cs
+++ Devtech2/Assets/_Scripts/Antivirus/AntivirusMovement.cs
@@ -12,6 +12,9 @@ public class AntivirusMovement : MonoBehaviour
     private Vector2 _dir;
     private Vector3Int _targetPosition;
 
+    // similar ao _inputTransform que tem no player
+    private Transform _movementTransform;
+
     [SerializeField] private float _speed = 4.0f;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private LayerMask boxMask;
@@ -21,11 +24,15 @@ public class AntivirusMovement : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _dir = Vector2.right;
+        _movementTransform = transform.GetChild(0);
+        _movementTransform.parent = null;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        CheckGround();
+        CheckForMovement();
         Movement();
     }
 
@@ -37,6 +44,8 @@ public class AntivirusMovement : MonoBehaviour
             return;
         _lastDirection = Mathf.Round(_dir.x);
         CheckAndMoveBoxes();
+        _movementTransform.position = transform.position + (Vector3)_dir;
+        _movementTransform.position = Vector3Int.FloorToInt(_movementTransform.position);
         var positionToInt = transform.position + (Vector3)_dir;
         _targetPosition = Vector3Int.FloorToInt(positionToInt);
 
@@ -49,13 +58,28 @@ public class AntivirusMovement : MonoBehaviour
     {
         if (_isGrounded)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, Time.deltaTime * _speed);
+            Debug.Log("MEXEU");
+            transform.position = Vector3.MoveTowards(transform.position, _movementTransform.position, Time.deltaTime * _speed);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.down * Time.deltaTime * _speed, Time.deltaTime * _speed);
         }
         if (transform.position == _targetPosition)
             _isMoving = false;
         else
             _isMoving = true;
         return;
+    }
+
+    private void MoveDown()
+    {
+        // Define a velocidade de queda
+        float fallSpeed = 5.0f;
+        // Calcula a nova posição para baixo
+        Vector3 downPosition = transform.position + Vector3.down * Time.deltaTime * fallSpeed;
+        // Move o objeto para a nova posição
+        transform.position = Vector3.MoveTowards(transform.position, downPosition, Time.deltaTime * fallSpeed);
     }
 
     private void CheckAndMoveBoxes()
@@ -66,6 +90,33 @@ public class AntivirusMovement : MonoBehaviour
         {
             hit.collider.gameObject.GetComponent<Box>().Move(new Vector2(_lastDirection, 0f));
             Flip();
+        }
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit2D hitL = Physics2D.Raycast(transform.position - new Vector3(0.44f, 0f, 0f), -transform.up, .55f, groundMask);
+        RaycastHit2D hitR = Physics2D.Raycast(transform.position + new Vector3(0.44f, 0f, 0f), -transform.up, .55f, groundMask);
+
+        if ((hitL.collider != null || hitR.collider != null))
+        {
+            if (transform.position.x % 1 <= 0.5f)
+                _targetPosition = Vector3Int.FloorToInt(transform.position);
+            else
+                _targetPosition = Vector3Int.CeilToInt(transform.position);
+
+            //_inputTransform.position = _targetPosition;
+            _isGrounded = true;
+        }
+        else
+        {
+            if (transform.position.x % 1 > 0.5f)
+                _targetPosition = Vector3Int.CeilToInt(transform.position + Vector3.down * .55f);
+            else
+                _targetPosition = Vector3Int.FloorToInt(transform.position + Vector3.down * .55f);
+
+            _movementTransform.position = _targetPosition + Vector3.up;
+            _isGrounded = false;
         }
     }
 
