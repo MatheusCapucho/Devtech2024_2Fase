ffbe5d676eefecbf32b593293b346c669d13073f
MatheusCapucho <matheusfcapucho@gmail.com>
2024-06-20 12:54:20 -0300

    Tile Coloring Max

--- Devtech2/Assets/_Scripts/GameManager.cs
+++ Devtech2/Assets/_Scripts/GameManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update    
-    void Start()
+    public static int VirusTiles = 0;
+
+    private void Awake()
     {
-        
+        VirusTiles = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Devtech2/Assets/_Scripts/Player/TileColoring.cs
+++ Devtech2/Assets/_Scripts/Player/TileColoring.cs
@@ -9,6 +9,12 @@ public class TileColoring : MonoBehaviour
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private Tile _virusTile;
+    [SerializeField] private int _maxColors = 99;
+
+    private void Awake()
+    {
+        tilemap = GameObject.Find("Ground").GetComponent<Tilemap>();
+    }
 
     private void FixedUpdate()
     {
@@ -23,10 +29,15 @@ public class TileColoring : MonoBehaviour
 
         var hitPoint = tilemap.WorldToCell(hit.point) + Vector3Int.down;
 
-        if(!tilemap.GetSprite(hitPoint) != _virusTile.sprite)
+        if(tilemap.GetSprite(hitPoint) != _virusTile.sprite && _maxColors > 0)
         {
-
             tilemap.SetTile(hitPoint, _virusTile);
+            GameManager.VirusTiles++;
+            _maxColors--; 
+            if(_maxColors <= 0)
+            {
+
+            }
         }
         
 
